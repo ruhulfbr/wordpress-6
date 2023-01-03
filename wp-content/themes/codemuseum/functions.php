34442a07427b4792b72e9ef469c865997d7a2693
ruhulfbr <ruhul11bd@gmail.com>
2023-01-03 12:59:02 +0600

    Add comment in function.php

--- wp-content/themes/codemuseum/functions.php
+++ wp-content/themes/codemuseum/functions.php
@@ -119,7 +119,7 @@ add_action('customize_register', 'cdm_customize_register');
 register_nav_menu('main_menu', __('Main Menu', 'codemuseum') );
 
 
-// Walker Menu Properties
+// Walker Menu Properties (Have to understand deeeply)
 function cdm_nav_description( $item_output, $item, $args){
   if( !empty ($item->description)){
     $item_output = str_replace($args->link_after . '</a>', '<span class="walker_nav">' . $item->description . '</span>' . $args->link_after . '</a>', $item_output);
