dae5913b056c569abdf1a64cd8a9f14975d0f733
ruhulfbr <ruhul11bd@gmail.com>
2023-01-05 12:36:03 +0600

    404

--- wp-content/themes/codemuseum/404.php
+++ wp-content/themes/codemuseum/404.php
@@ -9,9 +9,14 @@
                 <i class="bi bi-exclamation-triangle display-1 text-primary"></i>
                 <h1 class="display-1">404</h1>
                 <h1 class="mb-4">Page Not Found</h1>
+
+                <div class="error_search mt-1 mb-2">
+                    <?php get_search_form(); ?>
+                </div>
+
                 <p class="mb-4">We’re sorry, the page you have looked for does not exist in our website! Maybe go to
                     our home page or try to use a search?</p>
-                <a class="btn btn-primary py-3 px-4" href="">Go Back To Home</a>
+                <a class="btn btn-primary py-3 px-4" href="<?php echo home_url(); ?>">Go Back To Home</a>
             </div>
         </div>
     </div>
