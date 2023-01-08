cf0a722d2fbd4b859643df328731ab480f702c5a
ruhulfbr <ruhul11bd@gmail.com>
2023-01-09 00:21:13 +0600

    custom post tyoe category

--- wp-content/themes/codemuseum/inc/custom_post_type.php
+++ wp-content/themes/codemuseum/inc/custom_post_type.php
@@ -45,8 +45,9 @@ function cdm_custom_post_service() {
     'rewrite'            => array( 'slug' => 'service' ),
     'capability_type'    => 'post',
     'has_archive'        => true,
-    'hierarchical'       => false,
+    'hierarchical'       => true,
     'menu_position'      => null,
+    'taxonomies'         => ['category', 'year'],
     'supports'           => array( 'title', 'editor', 'thumbnail', 'custom-fields' ),
   );
 
@@ -55,5 +56,16 @@ function cdm_custom_post_service() {
 
 add_action( 'init', 'cdm_custom_post_service' );
 
-
-?>
+function query_post_type($query){
+  if(is_category() ){
+    $post_type = get_query_var('post_type');
+    if($post_type){
+      $post_type = $post_type;
+    } else {
+      $post_type = array ('post', 'service');
+      $query -> set('post_type', $post_type);
+      return $query;
+    }
+  }
+}
+add_filter('pre_get_posts', 'query_post_type');
