d55957648c041e8f514a1dcc0a2e2811554cb87e
ruhulfbr <ruhul11bd@gmail.com>
2023-01-12 14:53:05 +0600

    Custiom meta box

--- wp-content/themes/codemuseum/assets/js/custom.js
+++ wp-content/themes/codemuseum/assets/js/custom.js
@@ -1,6 +1,78 @@
-(function ($) {
-    "use strict";
+jQuery(function ($) {
+      /*
+       * Select/Upload image(s) event
+       */
+  $('body').on('click', '.cxc_multi_upload_image_button', function (e) {
+    e.preventDefault();
 
-    
-})(jQuery);
+    var button = $(this),
+    custom_uploader = wp.media({
+      title: 'Insert image',        
+      button: {
+        text: 'Use this image' 
+      },
+      multiple: true 
+    });
+    custom_uploader.on('select', function () { 
+      var attech_ids = '';
+      attachments
+      var attachments = custom_uploader.state().get('selection'),
+      attachment_ids = new Array(),
+      i = 0;
+      $(button).siblings('ul').empty();
+      attachments.each(function (attachment) {
+        attachment_ids[i] = attachment['id'];
+        attech_ids += ',' + attachment['id'];
+        if (attachment.attributes.type == 'image') {
+          $(button).siblings('ul').append('<li data-attechment-id="' + attachment['id'] + '"><a href="' + attachment.attributes.url + '" target="_blank"><img class="true_pre_image" src="' + attachment.attributes.url + '" /></a><i class=" dashicons  dashicons-no delete-img"></i></li>');
+        } else {
+          $(button).siblings('ul').append('<li data-attechment-id="' + attachment['id'] + '"><a href="' + attachment.attributes.url + '" target="_blank"><img class="true_pre_image" src="' + attachment.attributes.icon + '" /></a><i class=" dashicons  dashicons-no delete-img"></i></li>');
+        }
 
+        i++;
+      });
+
+      $(button).siblings('.attechments-ids').attr('value', attachment_ids);
+      $(button).siblings('.cxc_multi_remove_image_button').show();
+
+    });
+    custom_uploader.on('open',function() {
+      var selection = custom_uploader.state().get('selection');
+      var ids_value = $(button).siblings('.attechments-ids').val();
+
+      if(ids_value.length > 0) {
+        var ids = ids_value.split(',');
+
+        ids.forEach(function(id) {
+          attachment = wp.media.attachment(id);
+          attachment.fetch();
+          selection.add(attachment ? [attachment] : []);
+        });
+      }
+    });
+    custom_uploader.open();
+  });
+
+      /*
+       * Remove image event
+       */
+  $('body').on('click', '.cxc_multi_remove_image_button', function () {
+    $(this).hide().prev().val('').prev().addClass('button').html('Add  Media');
+    $(this).parent().find('ul').empty();
+    return false;
+  });
+
+  jQuery(document).on('click', '.cxc-multi-upload-medias ul li i.delete-img', function () {
+    var ids = [];
+    var attach_id =  jQuery(this).parents('li').attr('data-attechment-id');
+    jQuery('.cxc-multi-upload-medias ul li').each(function () {
+      if( attach_id != jQuery(this).attr('data-attechment-id') ){
+        ids.push(jQuery(this).attr('data-attechment-id'));  
+      }
+
+    });
+    jQuery(this).parents('.cxc-multi-upload-medias').find('input[type="hidden"]').val(ids);
+    jQuery(this).parent().remove();               
+  });
+
+});
--- wp-content/themes/codemuseum/functions.php
+++ wp-content/themes/codemuseum/functions.php
@@ -38,3 +38,145 @@ include_once('inc/bekrey_shortcode.php');
 
 // WP Bekrey Short Code
 include_once('inc/bekrey_shortcode_elements.php');
+
+
+
+
+
+ function wporg_add_custom_box() {
+        $screens = [ 'service' ];
+        foreach ( $screens as $screen ) {
+            add_meta_box(
+                'wporg_box_id',                 // Unique ID
+                'Custom Meta Box Title',      // Box title
+                'wporg_custom_box_html',  // Content callback, must be of type callable
+                $screen                            // Post type
+            );
+        }
+    }
+    function wporg_custom_box_html( $post ) {
+        $value = get_post_meta( $post->ID, 'service_price', true );
+        ?>
+        <label for="ems_product_price">Price</label>
+        <input type="text" name="ems_product_price" id="ems_product_price" class="postbox" value="<?= $value ?>" placeholder="Price">
+        <?php
+    }
+    add_action( 'add_meta_boxes', 'wporg_add_custom_box' );
+    function wporg_save_postdata( $post_id ) {
+        if ( array_key_exists( 'ems_product_price', $_POST ) ) {
+            update_post_meta(
+                $post_id,
+                'service_price',
+                $_POST['ems_product_price']
+            );
+        }
+    }
+    add_action( 'save_post', 'wporg_save_postdata' );
+
+
+
+add_filter( 'wpex_gallery_metabox_post_types', function( $types ) {
+	$types[] = 'service';
+	return $types;
+});
+
+
+
+add_action( 'add_meta_boxes', 'cxc_media_uploader_meta_box' );
+function cxc_media_uploader_meta_box() {
+	add_meta_box( 'cxc-media-field', 'Media Field', 'cxc_media_uploader_meta_box_func', 'service', 'normal', 'high' );
+}
+
+
+function cxc_media_uploader_meta_box_func( $post ) {
+
+	$banner_img = get_post_meta( $post->ID, 'cxc_post_banner_img', true );
+	?>
+	<table cellspacing="10" cellpadding="10">
+		<tr>
+			<td>Banner Image</td>
+			<td>
+				<?php echo cxc_multi_media_uploader_field( 'cxc_post_banner_img', $banner_img ); ?>
+			</td>
+		</tr>
+	</table>
+	<?php
+
+}
+function cxc_multi_media_uploader_field( $name, $value = '' ) {
+
+	$image = '">Add Media';
+	$image_str = '';
+	$image_size = 'full';
+	$display = 'none';
+	$value = explode( ',', $value );
+
+	if ( !empty( $value ) ) {
+		foreach ( $value as $values ) {
+			if ( $image_attributes = wp_get_attachment_image_src( $values, $image_size ) ) {
+				$image_str .= '<li data-attechment-id=' . $values . '><a href="' . $image_attributes[0] . '" target="_blank"><img src="' . $image_attributes[0] . '" /></a><i class="dashicons dashicons-no delete-img"></i></li>';
+			}
+		}
+	}
+
+	if($image_str){
+		$display = 'inline-block';
+	}
+	return '<div class="cxc-multi-upload-medias"><ul>' . $image_str . '</ul><a href="#" class="cxc_multi_upload_image_button button' . $image . '</a><input type="hidden" class="attechments-ids ' . $name . '" name="' . $name . '" id="' . $name . '" value="' . esc_attr( implode( ',', $value ) ) . '" /><a href="#" class="cxc_multi_remove_image_button button" style="display:inline-block;display:' . $display . '">Remove media</a></div>';
+
+}
+
+// Save Meta Box values.
+add_action( 'save_post', 'cxc_meta_box_save' );
+
+function cxc_meta_box_save( $post_id ) {
+
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ){
+		return;
+	}
+
+	// Check the user's permissions.
+	if ( ! current_user_can( 'edit_post', $post_id ) ){
+		return;
+	}
+	
+	if( isset( $_POST['cxc_post_banner_img'] ) ){
+		update_post_meta( $post_id, 'cxc_post_banner_img', $_POST['cxc_post_banner_img'] );
+	}
+}
+
+
+function cxc_add_admin_media_scripts() {
+	
+	wp_enqueue_style( 'admin-custom-style', get_template_directory_uri().'/assets/css/custom.css', false, '1.1', 'all' );
+	if ( ! did_action( 'wp_enqueue_media' ) ) {
+		wp_enqueue_media();
+	}
+	wp_enqueue_script('admin-custom-script', get_template_directory_uri().'/assets/js/custom.js', NULL, NULL, true);
+
+}
+add_action( 'admin_enqueue_scripts', 'cxc_add_admin_media_scripts' );
+
+
+function service_custom_columns( $columns ) {
+    $columns = array(
+        'cb' => '<input type="checkbox" />',
+        'featured_image' => 'Image',
+        'title' => 'Title',
+        'comments' => '<span class="vers"><div title="Comments" class="comment-grey-bubble"></div></span>',
+        'date' => 'Date'
+     );
+    return $columns;
+}
+add_filter('manage_service_posts_columns' , 'service_custom_columns');
+
+function service_custom_columns_data( $column, $post_id ) {
+    switch ( $column ) {
+    case 'featured_image':
+        the_post_thumbnail( 'thumbnail' );
+        break;
+    }
+}
+add_action( 'manage_service_posts_custom_column' , 'service_custom_columns_data', 10, 2 ); 
+
+
